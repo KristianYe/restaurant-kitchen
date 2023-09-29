84282e9ff5957dcd2f56d3c22d35fd77709ceaf7
Kristian Yendrek <kristianendreck4@gmail.com>
2023-09-29 17:37:46 +0300

    Added view for home page & base, index templates

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+
+from kitchen.views import index
 
 urlpatterns = [
-
+    path("", index, name="index"),
 ]
-
 
 app_name = "kitchen"
