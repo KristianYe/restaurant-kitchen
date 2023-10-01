70378b37c0d8341f54c43c44d977aa81c244e46c
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-01 13:55:00 +0300

    added assign button to dish detail

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -11,18 +11,20 @@ from kitchen.views import (
     DishCreateView,
     DishTypeCreateView,
     IngredientCreateView,
+    toggle_assign_to_dish
 )
 
 urlpatterns = [
     path("", index, name="index"),
-    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("toggle/", DishListView.as_view(), name="dish-list"),
     path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
-    path("dishes/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/<int:pk>/toggle-assign/", toggle_assign_to_dish, name="toggle-dish-assign"),
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
     path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
-    path("dish-types/<int:pk>", DishTypeDetailView.as_view(), name="dish-type-detail"),
+    path("dish-types/<int:pk>/", DishTypeDetailView.as_view(), name="dish-type-detail"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
-    path("cooks/<int:pk>", CookDetailView.as_view(), name="cook-detail"),
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
     path("ingredient_create/", IngredientCreateView.as_view(), name="ingredient-create")
     ]
 
