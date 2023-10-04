b9447eb73aa7936d816680d87240b61318090faa
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-04 19:28:48 +0300

    some improves & add test_user to readme.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # restaurant-kitchen
 Data for authentication:
-<li>Login: admin</li>
-<li>Password: admin</li>
+<li>Login: test_user</li>
+<li>Password: JVV52zGB</li>
--- kitchen/views.py
+++ kitchen/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q, Count
-from django.http import HttpResponseRedirect, Http404
+from django.http import Http404
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
