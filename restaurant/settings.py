dde8a66e2c62b3638daaccc113477346b1bb83fe
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-16 12:53:50 +0300

    added kitchen-manage-service.onrender.com to ALLOWED_HOSTS

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "kitchen-manage-service.onrender.com"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
